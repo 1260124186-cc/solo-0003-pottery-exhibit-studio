1f21c15bbc464409952467dd493c4fc7b17c0357
zhangchengcheng <zhangchengcheng@seentao.com>
2026-09-09 09:15:57 +0800

    feat: initialize solo-0003-pottery-exhibit-studio baseline

---
+++ src/App.vue
@@ -0,0 +1,1 @@
+<script setup lang='ts'>import StudioView from './views/StudioView.vue'</script><template><StudioView/></template>
---
+++ src/composables/useExhibitionFlow.ts
@@ -0,0 +1,1 @@
+import {computed} from 'vue';import {useExhibitService} from '../services/exhibitService';export function useExhibitionFlow(){const svc=useExhibitService();const totalPieces=computed(()=>svc.state.value.exhibitions.reduce((n:number,e:any)=>n+e.pieces.length,0));return{...svc,totalPieces}}
